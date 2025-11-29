c458d599a2580c137d5e3570f99735a36f6389bf
NicolasMazzey <nicolasmazzey@gmail.com>
2025-11-29 13:45:40 -0300

    CI/CD 11

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
 
   use: {
     baseURL: 'http://localhost:3000',
